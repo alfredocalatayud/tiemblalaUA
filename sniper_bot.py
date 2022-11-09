bd860ae5551ee0f923525b205ca21e00fe1672a1
Alfredo Calatayud <acs128@gcloud.ua.es>
2022-11-09 21:08:10 +0100

    Add mail(). Modify execute(), stop program and output. Modify select_dat(), check today.

--- sniper_bot.py
+++ sniper_bot.py
@@ -7,6 +7,9 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from threading import Thread
 from datetime import datetime, timedelta
+from email.message import EmailMessage
+import smtplib
+import ssl
 import time
 import pandas as pd
 
@@ -19,6 +22,32 @@ DICTIONARY = dict(
      ("15:00", "9"), ("16:00", "10"), ("17:00", "11"), ("18:00", "12"), ("19:00", "13"), ("20:00", "14"),
      ("21:00", "15"), ("22:00", "16")])
 PRIORITY = ["19:00", "18:00", "20:00", "21:00", "17:00"]
+
+# EMAIL_SENDER = 'acalatayuds@gmail.com'
+# EMAIL_PASSWORD = 'gypbdvklccuuwris'
+
+EMAIL_SENDER = '4lfredo98@gmail.com'
+EMAIL_PASSWORD = 'odrfhfducfckpcri'
+
+EMAIL_RECEIVER = 'acalatayuds@gmail.com'
+
+
+def mail(message):
+    subject = '¡Hay días disponibles!'
+    body = message
+
+    em = EmailMessage()
+
+    em['From'] = EMAIL_SENDER
+    em['To'] = EMAIL_RECEIVER
+    em['Subject'] = subject
+    em.set_content(body)
+
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+        smtp.login(EMAIL_SENDER, EMAIL_PASSWORD)
+        smtp.sendmail(EMAIL_SENDER, EMAIL_RECEIVER, em.as_string())
 
 
 class Temp(Thread):
@@ -53,10 +82,13 @@ class Temp(Thread):
             # Compare current hour with execution and execute or not function
             # If it has executed, one day added to objective date
             if hora <= datetime.now():
-                self.function()
+                output = self.function()
                 print('Ejecución programada ejecutada el {} a las {}'.format(hora.date(), hora.time()))
                 hora += timedelta(seconds=10)
-                print('Próxima ejecución programada el {} a las {}'.format(hora.date(), hora.time()))
+                if not output:
+                    print('Próxima ejecución programada el {} a las {}'.format(hora.date(), hora.time()))
+                else:
+                    self.stop()
 
             # Wait x seconds to restart
             sleep(self.delay)
@@ -65,11 +97,6 @@ class Temp(Thread):
 
 
 # 2to16 hours 8:00-22:00
-def execute():
-    print("Escogiendo hora")
-    driver = init_driver()
-    get_best_hour("11", driver)
-
 
 def login(driver, username, password):
     driver.get(LOGIN)
@@ -82,19 +109,25 @@ def login(driver, username, password):
 def select_day(driver, day):
     WebDriverWait(driver, 5).until(
         expected_conditions.element_to_be_clickable((By.ID, "fecha"))).click()
-    calendar = driver.find_element(By.CLASS_NAME, "datepicker-days")
-    columns = calendar.find_elements(By.TAG_NAME, "td")
-
-    for cell in columns:
-        if cell.text == day:
-            cell.click()
-            break
-
-    sleep(0.2)
     WebDriverWait(driver, 5).until(
-        expected_conditions.element_to_be_clickable((By.ID, "fecha"))).click()
-    WebDriverWait(driver, 5).until(
-        expected_conditions.element_to_be_clickable((By.CSS_SELECTOR, "td.active"))).click()
+        expected_conditions.element_to_be_clickable((By.CSS_SELECTOR, "td.active")))
+    current_day = driver.find_element(By.CSS_SELECTOR, "td.active")
+    today = current_day.text
+
+    if today != day:
+        calendar = driver.find_element(By.CLASS_NAME, "datepicker-days")
+        columns = calendar.find_elements(By.TAG_NAME, "td")
+
+        for cell in columns:
+            if cell.text == day:
+                cell.click()
+                break
+
+        sleep(0.2)
+        WebDriverWait(driver, 5).until(
+            expected_conditions.element_to_be_clickable((By.ID, "fecha"))).click()
+        WebDriverWait(driver, 5).until(
+            expected_conditions.element_to_be_clickable((By.CSS_SELECTOR, "td.active"))).click()
 
 
 def get_available_hours(driver, day):
@@ -114,7 +147,6 @@ def get_available_hours(driver, day):
         disponibles = tabla.find_elements(By.CSS_SELECTOR, "td.celdaDisponible")
 
         for disponible in disponibles:
-            print(disponible.text)
             output.append(disponible.text)
     except:
         print("No hay dias disponibles.")
@@ -126,7 +158,7 @@ def get_best_hour(day, driver=None):
     if not driver:
         driver = init_driver()
 
-    #login(driver, USERNAME, PASSWORD)
+    # login(driver, USERNAME, PASSWORD)
 
     driver.get(BODYBUILDING)
 
@@ -162,8 +194,13 @@ def get_best_hour(day, driver=None):
 def init_driver():
     options = webdriver.FirefoxOptions()
 
-    options.add_argument('--start-maximiezed')
-    options.add_argument('--disable-extensions')
+    # Maximized
+    # options.add_argument('--start-maximized')
+    # options.add_argument('--disable-extensions')
+
+    # Minimized
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
 
     driver = webdriver.Firefox(options=options)
 
@@ -229,8 +266,9 @@ def getCurrentReservations(driver):
 
         reservations.append(date)
 
-    except:
-        print("No encuentro el dia.")
+    except Exception as inst:
+        print("ERROR: {}\n".format(inst))
+        print("No se encuentró el dia.")
         day1 = False
 
     try:
@@ -247,19 +285,41 @@ def getCurrentReservations(driver):
 
         reservations.append(date)
 
-    except:
-        print("No encuentro el dia.")
+    except Exception as inst:
+        print("ERROR: {}\n".format(inst))
+        print("No se encuentró el dia.")
         day1 = False
 
     return reservations
 
 
+def execute():
+    message2 = ''
+
+    print("Buscando horas disponibles")
+    driver = init_driver()
+    driver.minimize_window()
+    horas = get_available_hours(driver, "11")
+
+    if horas:
+        message1 = 'Las siguientes horas están diponibles: \n\n'
+        for hora in horas:
+            message2 = message2 + '    -El día 11/11 a las {}.\n'.format(hora)
+        print('¡Hay horas disponible! Enviando correo...\n' + message2)
+        mail(message1 + message2)
+
+    driver.close()
+    return horas
+
+
 def main():
-    # t = Temp('18:25:00', 1, execute)
-    # t.start()
-    #
-    # sleep(1800)
-    # t.stop()
+    t = Temp('18:25:00', 1, execute)
+    t.start()
+
+    while t.is_alive():
+        sleep(10)
+
+    t.stop()
     # driver = init_driver()
 
     # reservations = getCurrentReservations(driver)
@@ -272,20 +332,17 @@ def main():
     # date = datetime.now()
     # print(date.time())
 
-    driver = init_driver()
-
-    removeReservation(driver, 0)
-
-    get_best_hour("11", driver)
+    # driver = init_driver()
+    #
+    # removeReservation(driver, 0)
+    #
+    # get_best_hour("11", driver)
     # availables = get_available_hours(driver, "11")
     #
     # for i in availables:
     #     if i in PRIORITY:
     #         get_best_hour("11", driver)
     #         break
-
-    print("OK")
-
 
 if __name__ == "__main__":
     main()
